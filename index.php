6453209db395637a93c997c23fd3cc8e9b8c5dab
Renato Druzian <rendruzian@gmail.com>
2020-08-11 20:44:24 -0300

    Projeto em php, necessita refatoração total

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content>
+		<meta name="author" content>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>+ Bixo - Home</title>
+		<link rel="stylesheet" href="./CSS/index.css"/>
+		<link rel="stylesheet" href="./Bootstrap/css/bootstrap.css"/>
+		<link rel="stylesheet" href="./Bootstrap/css/bootstrap-responsive.css"/>
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="./Bootstrap/js/bootstrap.min.js"></script>
+		<link href="../assests/ico/apple-touch-icon-144-precomposed.png" size="144x144" rel="apple-touch-icon-precomposed"></link>
+		<link href="../assests/ico/favicon.ico"></link>
+		<link href="../assests/ico/apple-touch-icon-144-precomposed.png" size="144x144" rel="apple-touch-icon-precomposed"></link>
+		<link href="../assests/ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-ico-precomposed"></link>
+	</head>
+
+	<body>
+		<div id="menu">
+			
+
+		</div>
+	</body>
+</html>
